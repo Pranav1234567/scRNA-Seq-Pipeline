785fcb7d2b7cbf9ced2c4d6e4096bf6772443679
Pranav1234567 <pnpiano@gmail.com>
2017-08-18 15:19:28 -0400

    updated bowtie2.sh

--- Alignment/bowtie2.sh
+++ Alignment/bowtie2.sh
@@ -13,24 +13,23 @@ NUMCELLS=$6
 i=1
 while ((i <= $NUMCELLS))
 do
-		for f in $(ls ${1}/$(basename ${3}_2)/trimCell.$i.fastq)
-		do
+	for f in $(ls ${1}/$(basename ${3}_2)/trimCell.$i.fastq)
+	do
 
-			mkdir ${4}/bowtie2/cell$i
+		mkdir ${4}/bowtie2/cell$i
   			
-			echo "Beginning Alignment using BOWTIE2..."
-                        bowtie2 -q --local -t --un ${4}/bowtie2/cell$i/un.fastq --al ${4}/bowtie2/cell$i/al.fastq -p 8 --qc-filter -x ${2}/Bowtie2Index/genome -U ${f} -S ${4}/bowtie2/cell$i/output.sam
-			echo "DONE"
+		echo "Beginning Alignment using BOWTIE2..."
+                bowtie2 -q --local -t --un ${4}/bowtie2/cell$i/un.fastq --al ${4}/bowtie2/cell$i/al.fastq -p 8 --qc-filter -x ${2}/Bowtie2Index/genome -U ${f} -S ${4}/bowtie2/cell$i/output.sam
+		echo "DONE"
 
-			done 
-			samtools view -bS ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/output.bam
-			bamtools stats -in ${4}/bowtie2/cell$i/output.bam > ${4}/bowtie2/cell$i/align_summary.txt
-			samtools view -b -F 4 ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/mapped.bam
-			samtools view -b -f 4 ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/unmapped.bam
-			samtools sort -n --threads 8 -o ${4}/bowtie2/cell$i/sorted_output.bam ${4}/bowtie2/cell$i/output.bam
-			rm ${4}/bowtie2/cell$i/output.sam
-			echo "outputted all the bam files"
-			echo "---------------------------"
-		done
+	done 
+	samtools view -bS ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/output.bam
+	bamtools stats -in ${4}/bowtie2/cell$i/output.bam > ${4}/bowtie2/cell$i/align_summary.txt
+	samtools view -b -F 4 ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/mapped.bam
+	samtools view -b -f 4 ${4}/bowtie2/cell$i/output.sam > ${4}/bowtie2/cell$i/unmapped.bam
+	samtools sort -n --threads 8 -o ${4}/bowtie2/cell$i/sorted_output.bam ${4}/bowtie2/cell$i/output.bam
+	rm ${4}/bowtie2/cell$i/output.sam
+	echo "outputted all the bam files"
+	echo "---------------------------"
 ((i+=1))
 done
